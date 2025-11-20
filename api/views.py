0fbefe46542506fc983c36428f734c7a7e73e5e7
Samuel <josephsegzy@gmail.com>
2025-11-20 19:43:37 +0100

    Fix DRF setup, add Book model, serializer, views, and urls

--- api/views.py
+++ api/views.py
@@ -1,8 +1,7 @@
-# api/views.py
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(generics.ListAPIView):
+class BookList(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
