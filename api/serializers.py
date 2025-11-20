549eb8e95196f847be1621a2e140329e144aaecb
Samuel <josephsegzy@gmail.com>
2025-11-20 20:26:06 +0100

    Fix DRF Book API: Add model, serializer, view, and urls

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,7 @@
-# my_api/serializers.py
 from rest_framework import serializers
 from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = '__all__'  # Include all fields
+        fields = '__all__'
