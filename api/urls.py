c97f63cdefd87849fa58f2783bf193b71c5dccc2
Samuel <josephsegzy@gmail.com>
2025-11-20 20:28:37 +0100

    Fix DRF Book API: Add model, serializer, view, and urls

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import BookList
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
+    path('books/', BookList.as_view(), name='book-list')
 ]
