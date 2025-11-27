2ff612100a89062eb287d15cf350e0404bba1b5b
Samuel <josephsegzy@gmail.com>
2025-11-27 19:47:24 +0100

    Initial commit: Django project with DRF and Book model

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+# A generic ListAPIView to list all books
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()         # Use the Book model as the queryset
+    serializer_class = BookSerializer     # Use the BookSerializer
