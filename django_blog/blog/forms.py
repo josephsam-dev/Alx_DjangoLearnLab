bf98db698188dd9d5c161db6013fbdf8200f6497
Samuel <josephsegzy@gmail.com>
2025-12-12 18:38:47 +0100

    Add PostForm with TagWidget for tagging, update models and forms

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,12 +1,16 @@
 from django import forms
-from .models import Comment
+from .models import Post, Comment
+from taggit.forms import TagWidget
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),
+        }
 
 class CommentForm(forms.ModelForm):
-    content = forms.CharField(
-        label='',
-        widget=forms.Textarea(attrs={'rows': 3, 'placeholder': 'Add a comment...'})
-    )
-
     class Meta:
         model = Comment
         fields = ['content']
