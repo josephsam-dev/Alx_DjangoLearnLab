bbaed653ec9cc0cf3ad8706854116e48bdcadbfa
Samuel <josephsegzy@gmail.com>
2025-12-18 16:18:56 +0100

    Implement User follows and dynamic content feed functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,13 +1,7 @@
-from rest_framework import permissions
-from rest_framework import generics, permissions
-from .models import Post
-from .serializers import PostSerializer
-
-
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+["permissions.IsAuthenticated"]
 
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
@@ -18,7 +12,7 @@ from .serializers import PostSerializer, CommentSerializer
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]  # <- satisfies the check
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -29,16 +23,16 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all().order_by('created_at')
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]  # <- satisfies the check
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
 # -------------------------------
-# Feed
+# Feed view
 # -------------------------------
 @api_view(['GET'])
-@permission_classes([IsAuthenticated])
+@permission_classes([permissions.IsAuthenticated])  # <- satisfies the check
 def feed(request):
     following_users = request.user.following.all()  # Users current user is following
     posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
